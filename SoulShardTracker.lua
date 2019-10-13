1fc22f0eeb1ea79cb35b3457c197c6b3477deb15
Trond Boksasp <trondboksasp@gmail.com>
2019-10-13 12:41:58 +0200

    Add MessageFrame to display number of soul shards

--- SoulShardTracker.lua
+++ SoulShardTracker.lua
@@ -1,4 +1,35 @@
-Sandbox = { }
+local function createStatusFrame()
+    local statusFrame = CreateFrame("Frame",nil,UIParent)
+    
+    -- Enable movable frame
+    statusFrame:SetMovable(true)
+    statusFrame:EnableMouse(true)
+    statusFrame:RegisterForDrag("LeftButton")
+    statusFrame:SetScript("OnDragStart", statusFrame.StartMoving)
+    statusFrame:SetScript("OnDragStop", statusFrame.StopMovingOrSizing)
+
+    -- Set size and position
+    statusFrame:SetWidth(110) 
+    statusFrame:SetHeight(20)
+    statusFrame:SetAlpha(0.8)
+    statusFrame:SetPoint("TOPLEFT",90,-5) -- above the character pane
+
+    -- Create texture
+    local texture = statusFrame:CreateTexture(nil,"BACKGROUND")
+    texture:SetAllPoints(true)
+    texture:SetColorTexture(0, 0, 0, 0.6)
+    statusFrame.texture = texture
+
+    -- Create text
+    statusFrame.text = statusFrame:CreateFontString(nil,"ARTWORK") 
+    statusFrame.text:SetFont("Fonts\\ARIALN.ttf", 13, "OUTLINE")
+    statusFrame.text:SetPoint("CENTER",0,0)
+    statusFrame:Show()
+
+    return statusFrame
+end
+
+local f1 = createStatusFrame()
 
 local function numberOfSoulShards()
     local soulShardID = 6265
@@ -10,16 +41,10 @@ local function numberOfSoulShards()
             end
         end
     end
-    print(soulShardCount)
+    f1.text:SetText(string.format("# of Soul Shards: %d", soulShardCount))
 end
 
-local finalNumberOfSoulShards = numberOfSoulShards()
-print(finalNumberOfSoulShards)
-
-
-local frame = CreateFrame("FRAME", "SandboxFrame");
-frame:RegisterEvent("BAG_UPDATE");
-local function eventHandler(self, event, ...)
- print("Handling event " .. event);
-end
-frame:SetScript("OnEvent", numberOfSoulShards);
+-- Create frame for handling bag update
+local bagUpdateHandler = CreateFrame("FRAME", "SandboxFrame");
+bagUpdateHandler:RegisterEvent("BAG_UPDATE");
+bagUpdateHandler:SetScript("OnEvent", numberOfSoulShards);
